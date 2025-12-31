8e579f90949ceb5b9b20c4e06d76d816b1e68af6
KunalSharma-IITBHU1 <kunal.student.cer23@itbhu.ac.in>
2025-12-31 18:14:39 +0530

    Update context.jsx

--- frontend/src/context/context.jsx
+++ frontend/src/context/context.jsx
@@ -10,7 +10,7 @@ const ContextProvider = ({ children }) => {
 
   const [token, setToken] = useState(localStorage.getItem("token"));
 
-  const url = "https://one0fooddelivery-backend.onrender.com/";
+  const url = "https://one0fooddelivery-backend.onrender.com";
 
   const fetchFoodList = async () => {
     try {
